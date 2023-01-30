f838ea47d35b59f9b582f210d09047eb8cfffbc4
Rafael Gonzaga <111755586+rgonzagaDTT@users.noreply.github.com>
2023-01-30 11:59:48 -0300

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -4,35 +4,35 @@ import boardgame.Position;
 
 public class ChessPosition {
 
-    private char column;
-    private int row;
+	private char column;
+	private int row;
 
-    public ChessPosition(char column, int row) {
-        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-            throw new ChessException("Error instantiating ChessPosition. Valid values are form a1 to h8");
-        }
-        this.column = column;
-        this.row = row;
-    }
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are form a1 to h8");
+		}
+		this.column = column;
+		this.row = row;
+	}
 
-    protected static ChessPosition fromPosition(Position position) {
-        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
-    }
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char) ('a' + position.getColumn()), 8 - position.getRow());
+	}
 
-    public char getColumn() {
-        return column;
-    }
+	public char getColumn() {
+		return column;
+	}
 
-    public int getRow() {
-        return row;
-    }
+	public int getRow() {
+		return row;
+	}
 
-    protected Position toPosition() {
-        return new Position(8 - row, column - 'a');
-    }
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
 
-    @Override
-    public String toString() {
-        return "" + column + row;
-    }
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
 }
